a4d97952a7fb66c4945d4925f776fe73ab962dec
Chamod Gamage <chamodgamage26@gmail.com>
2020-10-12 01:37:50 -0400

    Revert "Revert "Revert "update on page load"""
    This reverts commit 83fa3eba388e487245cc941962a95dc7752ae44c.

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -6,17 +6,10 @@ import StockForm from "./StockForm"
 import Header from "./Header"
 
 const StockList = (props) => {
-  const [trigger, setTrigger] = useState(false)
-  const { stocks, setUpdate, fetchStocks , setStocks} = useContext(PortfolioContext);
-  useEffect(() => { 
-    // console.log(JSON.parse(localStorage.getItem("stocks")));
-    // setStocks(JSON.parse(localStorage.getItem("stocks"))); 
-    fetchStocks(); 
-    // console.log('hi'); 
-    setTrigger(true)}
-  , [])
-  return ( 
-    !trigger ? <div/> :
+  const [trigger, setTrigger] = useState(0)
+  const { stocks, setUpdate, fetchStocks } = useContext(PortfolioContext);
+  useEffect(() => {fetchStocks(); setTrigger(0)}, [])
+  return (
     <div style = {{width: "100%"}}>
       <Header />
 
@@ -35,7 +28,7 @@ const StockList = (props) => {
             </div>
           </button>
       
-      {/* {console.log(stocks)} */}
+      {console.log(stocks)}
       <ul style = {{padding:0}}>
         {stocks?.map((stock, index) => {
           return <div style = {{paddingTop: 10, paddingBottom: 10}}>{console.log(index)}<StockDetails stock = {stock} index={index} /></div>;
