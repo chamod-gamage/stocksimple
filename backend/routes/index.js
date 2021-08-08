59b7f66ef6bd434d9ec985512a002346f277c208
Chamod Gamage <chamodgamage26@gmail.com>
2021-08-07 21:56:11 -0400

    add not found

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,5 +3,7 @@ import express from 'express';
 
 const app = express();
 app.use('/users', UserRouter);
-
+app.use('/', (request, response) => {
+  response.status(404).send({ message: 'NOT FOUND' });
+});
 export default app;
